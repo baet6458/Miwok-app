667c55e7787d030fc4712b36592eb760d7bd7883
elijsha Baetiong <ebaetiong2@gmail.com>
2019-05-15 11:27:04 -0400

    Added the rest of the app info since I forogt :(

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,10 +5,19 @@ public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
 
+    private int resourceid;
+
     public Word(String defaultTranslation, String miwokTranslation){
         mDefaultTranslation=defaultTranslation;
         mMiwokTranslation= miwokTranslation;
     }
+
+    public Word(String defaultTranslation, String miwokTranslation, int id){
+        mDefaultTranslation=defaultTranslation;
+        mMiwokTranslation= miwokTranslation;
+        resourceid=id;
+    }
+
 
     //return default translation
     public String getmDefaultTranslation() {
@@ -18,4 +27,5 @@ public class Word {
     public String getmMiwokTranslation() {
         return mMiwokTranslation;
     }
+    public int getResourceid(){return resourceid;}
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -6,16 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word>  {
 
     /**
      * Create a new {@link WordAdapter} object.
-     *
-     * @param context is the current context (i.e. Activity) that the adapter is being created in.
-     * @param words is the list of {@link Word}s to be displayed.
-     */
+     *  @param context is the current context (i.e. Activity) that the adapter is being created in.
+     * @param words is the list of {@link Word}s to be displayed.*/
     public WordAdapter(Context context, ArrayList<Word> words) {
         super(context, 0, words);
     }
