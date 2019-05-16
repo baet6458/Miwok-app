261ce9ffa1943ab80c8d57560eb7dd238315eab0
elijsha Baetiong <ebaetiong2@gmail.com>
2019-05-16 00:06:04 -0400

    Added audio and cleaned up the memory

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,24 +1,33 @@
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
+
 public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
 
+    private int mediaResource;
+
     private int resourceid=No_IMAGE_PROVIDED ;
 
     private static final int No_IMAGE_PROVIDED =-1;
 
-    public Word(String defaultTranslation, String miwokTranslation){
-        mDefaultTranslation=defaultTranslation;
-        mMiwokTranslation= miwokTranslation;
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mediaResource = audioResourceId;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int id){
-        mDefaultTranslation=defaultTranslation;
-        mMiwokTranslation= miwokTranslation;
-        resourceid=id;
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId,
+                int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        resourceid = imageResourceId;
+        mediaResource = audioResourceId;
     }
+
+
 
 
     //return default translation
@@ -34,5 +43,17 @@ public class Word {
     //return if a word has an image
     public boolean hasImage(){
         return resourceid!=No_IMAGE_PROVIDED;
+    }
+
+    public int getAudioResourceId(){return mediaResource;}
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mediaResource=" + mediaResource +
+                ", resourceid=" + resourceid +
+                '}';
     }
 }
